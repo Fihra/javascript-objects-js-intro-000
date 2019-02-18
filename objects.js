9b9ec2cd60f59e914fcedcb7de322c51784ee5d4
Fabian Fabro <fabrofabian@gmail.com>
2019-02-18 23:19:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,13 +2,6 @@ var playlist = {
   artistName: "songTitle"
 };
 
-/*
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
-  return playlist;
-}
-*/
-
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
